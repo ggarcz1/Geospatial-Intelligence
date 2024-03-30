11dbd2829e7fc17af1583888e12d167e88ddd81d
ggarcz1 <54447564+ggarcz1@users.noreply.github.com>
2024-03-29 20:28:54 -0500

    Update coords.py

--- coords.py
+++ coords.py
@@ -2,7 +2,7 @@ import math
 
 
 class Coords:
-    def __init__(self, x=0, y=0, z=0, dimension=0):
+    def __init__(self, x=0, y=0, z=0, dimension=0) -> None:
         self.x = x
         self.y = y
         self.z = z
