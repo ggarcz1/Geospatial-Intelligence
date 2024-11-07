2da62db23f6013898bbcdd7eb698d81109b38c99
ggarcz1 <54447564+ggarcz1@users.noreply.github.com>
2024-11-07 09:09:45 -0500

    updated auto map open
    added satellite, normal map views
    added find functionality
    added argparse for cli inputs

--- automation/auto_open_maps.py
+++ automation/auto_open_maps.py
@@ -2,10 +2,12 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+import argparse
 
 # source
-# https://chromedriver.storage.googleapis.com/index.html?path=113.0.5672.24/
+# https://googlechromelabs.github.io/chrome-for-testing/
 
+# python .\auto_open_maps.py -lat 38.89694433202297 -long -77.0366269543101 -zoom 13
 
 # default set to 0
 latitude = longitude = 0
@@ -20,10 +22,51 @@ longitude = coordinates[1]
 lat_look = latitude
 long_look = longitude
 
+parser = argparse.ArgumentParser(description='map lat, long, zoom')
+parser.add_argument('-lat', '--arg1', help='latituide')
+parser.add_argument('-long', '--arg2', help='longituide')
+parser.add_argument('-zoom', '--arg3', help='zoom')
+parser.add_argument('-view', '--arg4', help='view')
+
+
+args = parser.parse_args()
+if args.arg1:
+    latitude = args.arg1
+else:
+    latitude = latitude
+    
+if args.arg2:
+    longitude = args.arg2
+else:
+    longitude = longitude
+
 # double from 0 - 20
-zoom = 13
-# url = f'https://www.google.com/maps/@{latituide},{longituide},{zoom}z?entry=ttu'
-url = f'https://www.google.com/maps/place/0%C2%B000\'{latitude}%22N+0%C2%B000\'{longitude}%22E/@{lat_look},{long_look},{zoom}z?entry=ttu'
+if args.arg3:
+    zoom = args.arg3
+else:
+    zoom = 13
+
+# s --> satellite
+# n --> normal maps
+# f --> find location
+if args.arg4:
+    view = args.arg4
+else:
+    view = 's'
+
+
+if view == 'f':
+    # finds your location
+    url = f'https://www.google.com/maps/@None,None,1046m/data=!3m1!1e3?entry=ttu&g_ep=EgoyMDI0MTAyOS4wIKXMDSoASAFQAw%3D%3D'
+
+elif view == 'n':
+    # plain map
+    url = f'https://www.google.com/maps/@{latitude},{longitude},{zoom}z?entry=ttu'
+
+elif view == 's':
+    # satellite
+    url = f'https://www.google.com/maps/@{latitude},{longitude},1046m/data=!3m1!1e3?entry=ttu&g_ep=EgoyMDI0MTAyOS4wIKXMDSoASAFQAw%3D%3D'
+
 file_name = 'selenium_tests.py'
 
 # keep tab opened
